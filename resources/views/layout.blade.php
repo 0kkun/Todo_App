a4927f3671c6b84e7c0d6a77a41057111f631cdc
0kkun <shinji.okumoto.01@gmail.com>
2020-04-08 15:19:06 +0900

    correct

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>ToDo App</title>
-  <!-- ページごとに変化する部分は @yield で穴埋め -->
+  <!-- ページごとに変化する部分は ＠yield で穴埋め -->
   @yield('styles')
   <link rel="stylesheet" href="/css/styles.css">
 </head>
