43c57e5e2cde143ada13b116dd30e2dbd48f907c
0kkun <shinji.okumoto.01@gmail.com>
2020-04-08 19:44:20 +0900

    add seed file

--- database/seeds/FoldersTableSeeder.php
+++ database/seeds/FoldersTableSeeder.php
@@ -12,11 +12,15 @@ class FoldersTableSeeder extends Seeder
 
     public function run()
     {
+        // first メソッドでユーザーを一行だけ取得して、その ID を user_id の値に指定
+        $user = DB::table('users')->first();
+
         $titles = ['プライベート', '仕事', '旅行'];
 
         foreach ($titles as $title) {
             DB::table('folders')->insert([
                 'title' => $title,
+                'user_id' => $user->id,
                 'created_at' => '2020-04-03',
                 'updated_at' => '2020-04-03',
             ]);
